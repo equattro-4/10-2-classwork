8777ce995739406e2138858b70859f14f0173bf8
equattro-4 <eliquattrocchi@gmail.com>
2024-10-02 11:35:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-//joker makes a joke
+Properties
+Love % (shared between two players)
+Trust %
+Intimacy %
+Safety %
+Vulnerability %
+Communication %
+Compassion %
+Negative Emotions (these fall under the properties of love)
+ Anger %
+Resentment %
+Jealousy %
+Contempt
+ Regret
+All these emotions, the bad and also the good, work together to balance the percentage of the love bar.
+
+Events (between two players that affect love):
+Kiss
+Effects:
+Increases love % between the two players
+Increases intimacy %
+Increases compassion %
+Date
+Effects:
+Increases love %
+Increases intimacy %
+Increases communication %
+Increases vulnerability %
+Increases trust %
+Bad Argument
+Effects:
+Decreases trust, honesty, intimacy, vulnerability, communication, and compassion %
+Slightly increases anger, resentment, jealousy, and contempt %
+After a few time periods, regret % adds a little to the love %
+Good Argument
+Effects:
+Slightly increases trust, honesty, intimacy, vulnerability, communication, and compassion %
+Honesty
+Effects:
+Increases trust % greatly
+Slightly increases intimacy, compassion, and vulnerability %
+
+Effects
+Positive emotions increase love %, while negative emotions work to lower it. However, regret % may increase love % a bit later, once it’s reflected upon.
+Summary: This is a base coding model for love and how it works, but there are many other events and factors that can effect the properties of love, this is an incomplete draft and it would take much more long and thoughtful work to come up with a near complete model, but I dont think its possible to create a perfect coding model for love, because evertxy person is different and shares love with their partner in nuanced ways.
+
